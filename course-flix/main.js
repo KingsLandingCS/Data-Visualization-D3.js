11fe423fa6c0def048dee7dedaf697d7d5d4f964
KingsLandingCS <finalfantasy.fantasy9@gmail.com>
2024-08-20 15:23:22 +0500

    learning

--- course-flix/main.js
+++ course-flix/main.js
@@ -1,3 +1,13 @@
-const headerTag = document.querySelector("h1");
+// const headerTag = document.querySelector("h1");
 
-headerTag.innerHTML = "Hello Zubi!";
+// headerTag.innerHTML = "Hello Zubi!";
+// headerTag.style.backgroundColor = "var(--primary-orange)";
+// headerTag.style.fontSize = "64px";
+
+const headerTags = document.querySelectorAll("h1");
+
+headerTags.forEach((h1) => {
+
+    h1.style.backgroundColor = "#ffff00";
+    h1.style.fontSize = "42px";
+})
