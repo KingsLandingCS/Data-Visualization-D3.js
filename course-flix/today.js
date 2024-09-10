2c919ec1d3afd4bb32e1fcb072171fafddcae4b1
KingsLandingCS <finalfantasy.fantasy9@gmail.com>
2024-09-10 21:50:31 +0500

    learning

--- course-flix/today.js
+++ course-flix/today.js
@@ -1,14 +1,19 @@
-const todayData = [20, 30, 50, 6, 4, 2, 5, 20, 24, 32,
-    40, 59, 68, 89, 100, 112, 109, 101,
-    78, 56, 32, 31, 12, 1];
+const todayData = [20, 30, 50, 6, 4, 2,
+    5, 20, 24, 32, 40, 59, 68, 89, 100,
+    112, 109, 101, 78, 56, 32, 31, 12, 1];
 
 
 const todaySvg = d3.select("svg");
-const rectTags = d3.selectAll("rect");
 
-rectTags
+
+todaySvg
+    .selectAll("rect")
     .data(todayData)
+    .enter()
+    .append("rect")
     .attr("width", (d, i) => { return d })
-    .attr("y", (d, i) => { return 50 * i })
+    .attr("y", (d, i) => { return 20 * i })
     .attr("x", 0)
-    .arrt("height", 40)
+    .attr("height", 16);
+
+console.log(todaySvg.selectAll("rect").data(todayData).enter());
