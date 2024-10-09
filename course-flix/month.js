9a31b3a64ac6144a6650768fdb6dc9f2b95cfdee
KingsLandingCS <finalfantasy.fantasy9@gmail.com>
2024-10-09 13:44:03 +0500

    learning

--- course-flix/month.js
+++ course-flix/month.js
@@ -1,6 +1,9 @@
 import { monthData, todayData } from './data.js';
 
 const monthSvg = d3.select("svg.month");
+const radiusScale = d3.scaleSqrt()
+    .domain([0, 30000])
+    .range([0, 50])
 
 monthSvg
     .selectAll("circle")
@@ -9,4 +12,4 @@ monthSvg
     .append("circle")
     .attr("cx", (d, i) => { return (i % 7) * 120 + 60 })
     .attr("cy", (d, i) => { return Math.floor(i / 7) * 100 + 60 })
-    .attr("r", (d, i) => { return 5 })
+    .attr("r", (d, i) => { return radiusScale(d) })
